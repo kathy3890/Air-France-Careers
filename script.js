14577b20d2d8574f206bff6db0b8f19306360ae5
kathy3890 <cothompson6@gmail.com>
2025-05-04 21:43:37 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
+document.getElementById('app-form').addEventListener('submit', function (e) {
+  e.preventDefault();
+  alert('Thank you for applying! Our team will review your application soon.');
+});
 
